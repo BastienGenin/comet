0ae993e64efc6c9e4b7c2cac77daa427204a6aa8
Bastien Genin <bastien.genin@jdc.fr>
2024-03-20 22:06:29 +0100

    chore(lint): Add eslint & prettier

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 console.log("Hello via Bun!");
+
+// Path: index.ts
+export async function testJobProcess(server: string, job: number) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
